beef53f71cc9eee52d4016c7f1b2c716385ae0f6
Antonio <antonio@example.com>
2026-04-23 13:48:00 -0500

    parseo csv y validacion basica

--- src/repositorio.cpp
+++ src/repositorio.cpp
@@ -5,6 +5,45 @@
 
 using namespace std;
 
+static int textoaentero(string texto) {
+    int inicio = 0;
+    int fin = int(texto.size());
+
+    while (inicio < fin) {
+        char c = texto[inicio];
+        if (c == ' ') {
+            inicio = inicio + 1;
+        } else {
+            break;
+        }
+    }
+
+    while (fin > inicio) {
+        char c = texto[fin - 1];
+        if (c == ' ') {
+            fin = fin - 1;
+        } else {
+            break;
+        }
+    }
+
+    int valor = 0;
+    for (int i = inicio; i < fin; i = i + 1) {
+        char c = texto[i];
+        if (c < '0') {
+            break;
+        }
+
+        if (c > '9') {
+            break;
+        }
+
+        valor = (valor * 10) + int(c - '0');
+    }
+
+    return valor;
+}
+
 static int partirlinea(string linea, string campos[], int maxcampos) {
     int usados = 0;
     string actual = "";
@@ -35,14 +74,22 @@ static int partirlinea(string linea, string campos[], int maxcampos) {
 Repositorio::Repositorio() {
     filascsv = 0;
     columnascsv = 0;
+    cantidadequipos = 0;
     rutacsv = "";
+
+    for (int i = 0; i < maxequipos; i = i + 1) {
+        equiposcsv[i] = "";
+        confederacionescsv[i] = "";
+        rankingscsv[i] = 0;
+    }
 }
 
 void Repositorio::cargarBase() {
     bool ok = leercsvequipos("../../../selecciones_clasificadas_mundial.csv");
 
     if (ok) {
-        cout << "csv ok -> filas: " << filascsv << ", columnas: " << columnascsv << endl;
+        cout << "csv ok -> filas: " << filascsv << ", columnas: " << columnascsv;
+        cout << ", equipos: " << cantidadequipos << endl;
     } else {
         cout << "no encontre el csv" << endl;
     }
@@ -56,7 +103,14 @@ bool Repositorio::leercsvequipos(string ruta) {
 
     filascsv = 0;
     columnascsv = 0;
+    cantidadequipos = 0;
     rutacsv = ruta;
+
+    for (int i = 0; i < maxequipos; i = i + 1) {
+        equiposcsv[i] = "";
+        confederacionescsv[i] = "";
+        rankingscsv[i] = 0;
+    }
 
     string linea = "";
 
@@ -81,6 +135,26 @@ bool Repositorio::leercsvequipos(string ruta) {
             continue;
         }
 
+        string campos[40];
+        int totalcampos = partirlinea(linea, campos, 40);
+
+        bool filavalida = true;
+        if (totalcampos < 5) {
+            filavalida = false;
+        }
+
+        if (cantidadequipos >= maxequipos) {
+            filavalida = false;
+        }
+
+        if (filavalida) {
+            int pos = cantidadequipos;
+            rankingscsv[pos] = textoaentero(campos[0]);
+            equiposcsv[pos] = campos[1];
+            confederacionescsv[pos] = campos[4];
+            cantidadequipos = pos + 1;
+        }
+
         filascsv = filascsv + 1;
     }
 
@@ -94,4 +168,44 @@ int Repositorio::getfilascsv() const {
 
 int Repositorio::getcolumnascsv() const {
     return columnascsv;
+}
+
+int Repositorio::getcantidadequipos() const {
+    return cantidadequipos;
+}
+
+string Repositorio::getequipo(int indice) const {
+    if (indice < 0) {
+        return "";
+    }
+
+    if (indice >= cantidadequipos) {
+        return "";
+    }
+
+    return equiposcsv[indice];
+}
+
+string Repositorio::getconfederacion(int indice) const {
+    if (indice < 0) {
+        return "";
+    }
+
+    if (indice >= cantidadequipos) {
+        return "";
+    }
+
+    return confederacionescsv[indice];
+}
+
+int Repositorio::getranking(int indice) const {
+    if (indice < 0) {
+        return 0;
+    }
+
+    if (indice >= cantidadequipos) {
+        return 0;
+    }
+
+    return rankingscsv[indice];
 }
--- src/repositorio.h
+++ src/repositorio.h
@@ -13,11 +13,22 @@ public:
     bool leercsvequipos(string ruta);
     int getfilascsv() const;
     int getcolumnascsv() const;
+    int getcantidadequipos() const;
+    string getequipo(int indice) const;
+    string getconfederacion(int indice) const;
+    int getranking(int indice) const;
 
 private:
+    static const int maxequipos = 64;
+
     int filascsv;
     int columnascsv;
+    int cantidadequipos;
+
     string rutacsv;
+    string equiposcsv[maxequipos];
+    string confederacionescsv[maxequipos];
+    int rankingscsv[maxequipos];
 };
 
 #endif // REPOSITORIO_H
